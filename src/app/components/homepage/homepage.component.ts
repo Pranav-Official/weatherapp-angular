e1936865999893d3d7ced5d90daa89e48b46df28
Nigin N Manayil <niginnmanayil10@gmail.com>
2024-04-09 18:32:16 +0530

    Feat : Temperature Widget

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { TemperatureWidgetComponent } from '../temperature-widget/temperature-widget.component';
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [TemperatureWidgetComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
