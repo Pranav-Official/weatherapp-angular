c152eeb9ecbd390752d1264d2d42a15ccfd71fc1
Nigin N Manayil <niginnmanayil10@gmail.com>
2024-04-10 09:21:59 +0530

    Feat : Added Map Widget

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TemperatureWidgetComponent } from '../temperature-widget/temperature-widget.component';
+import { MapWidgetComponent } from '../map-widget/map-widget.component';
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [TemperatureWidgetComponent],
+  imports: [TemperatureWidgetComponent, MapWidgetComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
