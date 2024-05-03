553b340e21fe201a2bcefa2fe6285b78de224198
Nigin N Manayil <niginnmanayil10@gmail.com>
2024-05-03 14:15:07 +0530

    feat : login-signup - first testing done

--- src/app/components/login-signup/login-signup.component.spec.ts
+++ src/app/components/login-signup/login-signup.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginSignupComponent } from './login-signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('LoginSignupComponent', () => {
   let component: LoginSignupComponent;
@@ -8,7 +9,7 @@ describe('LoginSignupComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginSignupComponent, HttpClientModule],
+      imports: [LoginSignupComponent, HttpClientModule, ReactiveFormsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginSignupComponent);
@@ -18,5 +19,11 @@ describe('LoginSignupComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return the controls of the signupForm', () => {
+    const expectedControls = component.signupForm.controls;
+    const controls = component.f;
+    expect(controls).toEqual(expectedControls);
   });
 });
