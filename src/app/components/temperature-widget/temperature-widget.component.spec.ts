45dcd1d0d600d5827eab18bf1a02a3046acb1d7a
Nigin N Manayil <niginnmanayil10@gmail.com>
2024-05-08 10:53:25 +0530

    testing-phase-1

--- src/app/components/temperature-widget/temperature-widget.component.spec.ts
+++ src/app/components/temperature-widget/temperature-widget.component.spec.ts
@@ -8,16 +8,35 @@ describe('TemperatureWidgetComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TemperatureWidgetComponent]
-    })
-    .compileComponents();
-    
+      imports: [TemperatureWidgetComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TemperatureWidgetComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    jest.clearAllMocks(); // Clear all mock functions after each test
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return true if preferred_units is imperial', () => {
+    const localStorageMock: any = {
+      getItem: jest.fn().mockReturnValue('imperial'),
+    };
+    Object.defineProperty(window, 'localStorage', {
+      value: localStorageMock,
+    });
+
+    expect(component.isImperial()).toBeTruthy();
+  });
+
+  it('should return false if preferred_units is not imperial', () => {
+    jest.spyOn(localStorage, 'getItem').mockReturnValue('metric');
+    expect(component.isImperial()).toBeFalsy();
   });
 });
