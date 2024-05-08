b79ad6d4d72e52c833250aee3331ac47509276c9
Nigin N Manayil <niginnmanayil10@gmail.com>
2024-05-08 16:16:46 +0530

    feat : started navbar testing

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
 
@@ -8,16 +13,40 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
-    })
-    .compileComponents();
-    
+      imports: [NavbarComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  afterEach(async () => {
+    localStorage.clear();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should clear the access token from the local storage when the logout function is called (logout button is clicked', () => {
+    localStorage.setItem('accessToken', 'sampleToken');
+    component.logout();
+    expect(localStorage.getItem('accessToken')).toBeNull();
+  });
+
+  it('should return the loggedin status as true when checked for login status', () => {
+    localStorage.setItem('accessToken', 'sampleToken');
+    component.isLoggedIn();
+    expect(localStorage.getItem('accessToken')).not.toBeNull();
+  });
+
+  it('should set menuSelector property after a short delay', fakeAsync(() => {
+    const menuSelector = 'NEW_MENU';
+    component.setMenuSelector(menuSelector);
+    expect(component.menuSelector).toBe('SETTINGS');
+
+    tick(11); // To Simulate passage of time by calling tick() with a delay slightly longer than the setTimeout
+    expect(component.menuSelector).toBe(menuSelector);
+  }));
 });
